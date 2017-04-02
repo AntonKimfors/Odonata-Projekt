4cf2665c23213b8425ba8153adbeaddbd1385fc7
AntonKimfors <antonkimfors@hotmail.com>
2017-04-02 12:20:52 +0200

    Created Questioneer view

--- app/src/main/java/com/example/murk/kwizgeeq/Questioneer.java
+++ app/src/main/java/com/example/murk/kwizgeeq/Questioneer.java
@@ -2,14 +2,19 @@ package com.example.murk.kwizgeeq;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class Questioneer extends AppCompatActivity {
+
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.AppTheme_NoActionBar); // (for Custom theme)
+
         setContentView(R.layout.activity_questioneer);
+
     }
 }
