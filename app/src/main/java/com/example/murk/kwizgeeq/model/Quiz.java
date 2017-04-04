caeb924a3b811374422e4de6fff60bdcd29fad6e
gaffelman <henrikhakansson95@gmail.com>
2017-04-04 07:48:42 +0200

    basic functionality of Quiz class

---
+++ app/src/main/java/com/example/murk/kwizgeeq/model/Quiz.java
@@ -0,0 +1,44 @@
+package com.example.murk.kwizgeeq.model;
+
+import android.graphics.Color;
+
+import java.util.List;
+
+/**
+ * Created by Henrik on 04/04/2017.
+ */
+
+public class Quiz {
+    private List<Question> questions;
+    private Color listColor;
+    private String name;
+
+    public Quiz(String name,Color listColor) {
+        this.name = name;
+        this.listColor = listColor;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Color getListColor() {
+        return listColor;
+    }
+
+    public void setListColor(Color listColor) {
+        this.listColor = listColor;
+    }
+
+    public List<Question> getQuestions() {
+        return questions;
+    }
+
+    public void addQuestion(Question question) {
+        questions.add(question);
+    }
+}
