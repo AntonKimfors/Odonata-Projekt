d60ef3190c21f61eae042fdde469f04b8c72e999
AntonKimfors <antonkimfors@hotmail.com>
2017-04-05 05:31:18 +0200

    Added methods (ony header)and variables to KwizGeeQ class

--- app/src/main/java/com/example/murk/kwizgeeq/model/KwizGeeQ.java
+++ app/src/main/java/com/example/murk/kwizgeeq/model/KwizGeeQ.java
@@ -1,10 +1,22 @@
 package com.example.murk.kwizgeeq.model;
 
+import android.graphics.Color;
+
+import java.util.List;
+
 /**
  * Created by akimfors on 2017-04-05.
  */
 
 public class KwizGeeQ {
 
-    
+    public List<Quiz> quizzList;
+    public Quiz activeQuiz;
+
+
+    public void addNewQuiz(Color color){};
+
+    public void firePlayQuiz(Quiz quiz){};
+
+    public void fireEditQuiz(Quiz quiz){};
 }
