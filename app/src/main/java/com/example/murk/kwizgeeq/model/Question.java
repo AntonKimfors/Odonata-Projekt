79f1af076989d52cb0481201886654d2f54d0c91
gaffelman <henrikhakansson95@gmail.com>
2017-04-05 10:10:12 +0200

    Question has always list of 4 answers, if not any of them are null iterator can be returned

--- app/src/main/java/com/example/murk/kwizgeeq/model/Question.java
+++ app/src/main/java/com/example/murk/kwizgeeq/model/Question.java
@@ -2,47 +2,102 @@ package com.example.murk.kwizgeeq.model;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Created by Henrik on 04/04/2017.
  */
 
 public abstract class Question {
-    private List<Answer> answers;
 
-    int numberOfAnswers;
+    //the list of answers to question, size should always be 4
+    private final List<Answer> answers;
+
+    //keeps track of number of inserted answers
+    private int numberOfAnswers;
 
     public Question() {
-        answers = new ArrayList<>();
+        answers = new ArrayList<>(4);
         numberOfAnswers = 0;
     }
 
+    /**
+     * Shuffles the order of answers and returns iterator
+     * @return iterator over answers if question is valid, if not return null
+     */
     public Iterator<Answer> answerIterator() {
-        shuffleAnswers();
-        return answers.iterator();
+        if(isValid()){
+            //shuffle order of answers
+            Collections.shuffle(answers);
+            return answers.iterator();
+        }
+        return null;
     }
 
     public List<Answer> getAnswers(){
         return answers;
     }
 
-    public void addAnswer(Answer answer) {
-        if(numberOfAnswers<5){
-            answers.add(answer);
-            numberOfAnswers++;
+    /**
+     * Add a new unique answer to the answer list
+     * @param answer
+     * @return true if insert successful
+     */
+    public boolean addAnswer(Answer answer) {
+        //check if element already exists
+        if(answers.contains(answer)){
+            return false;
         }
+
+        //add answer last in list
+        if(numberOfAnswers<4){
+            answers.set(numberOfAnswers,answer);
+            numberOfAnswers++;
+            return true;
+        }
+
+        return false;
     }
 
-    public void removeAnswer(Answer answer){
-        answers.remove(answer);
+    /**
+     * Removes an answer and adds a null object in end of list
+     * @param answer
+     */
+    public boolean removeAnswer(Answer answer){
+        if(!answers.contains(answer))
+            return false;
+        int index = answers.indexOf(answer);
+        //size should always be 4
+        int size = answers.size();
+
+        //move all answer elements after the answer to remove one position forward
+        for(int i=index;i<answers.size()-2;i++){
+            answers.set(i,answers.get(i+1));
+        }
+
+        //add null to last position
+        answers.set(size-1,null);
+
+        numberOfAnswers--;
+
+        return true;
     }
 
-    private void shuffleAnswers(){
-        Collections.shuffle(answers);
+    /**
+     *
+     * @return true if numberOfAnswers is 4
+     */
+    public boolean isValid(){
+        return (numberOfAnswers==4) ? true : false;
     }
 
+    /**
+     * @param obj
+     * @return true if lists of answers are equal, independent of order
+     */
     @Override
     public boolean equals(Object obj) {
         if(obj == null){
@@ -50,9 +105,11 @@ public abstract class Question {
         }
         if(obj instanceof Question){
             Question o = (Question) obj;
-            if(answers.equals(o.getAnswers())){
-                return true;
-            }
+
+            Set<Answer> s1 = new HashSet<>(answers);
+            Set<Answer> s2 = new HashSet<>(o.getAnswers());
+
+            return s1.equals(s2);
         }
         return false;
     }
