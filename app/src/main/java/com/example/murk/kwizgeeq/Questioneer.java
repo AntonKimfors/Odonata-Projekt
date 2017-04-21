54538b0c783b7ca812310817bffc2f53664864b7
Are <are.ehnberg@outlook.com>
2017-04-21 15:39:45 +0200

    Finished functionality for showing questions and answers, added more answers to temporary test-quiz (needed to run correctly)

--- app/src/main/java/com/example/murk/kwizgeeq/Questioneer.java
+++ app/src/main/java/com/example/murk/kwizgeeq/Questioneer.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.murk.kwizgeeq.model.Answer;
@@ -13,7 +14,12 @@ import com.example.murk.kwizgeeq.model.Quiz;
 import com.example.murk.kwizgeeq.model.UserQuestion;
 import com.example.murk.kwizgeeq.model.UserQuiz;
 
+import java.util.Iterator;
+
 public class Questioneer extends AppCompatActivity {
+
+    private int curQuest;
+    private int totQuest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,15 +40,48 @@ public class Questioneer extends AppCompatActivity {
         testQuestion2.addAnswer(testCorrectAnswer);
         testQuestion3.addAnswer(testCorrectAnswer);
         testQuestion1.addAnswer(testWrongAnswer);
+        testQuestion1.addAnswer(testWrongAnswer);
+        testQuestion1.addAnswer(testWrongAnswer);
+        testQuestion2.addAnswer(testWrongAnswer);
+        testQuestion2.addAnswer(testWrongAnswer);
         testQuestion2.addAnswer(testWrongAnswer);
         testQuestion3.addAnswer(testWrongAnswer);
+        testQuestion3.addAnswer(testWrongAnswer);
+        testQuestion3.addAnswer(testWrongAnswer);
 
-        TextView quizLabel = (TextView) findViewById(R.id.quizLabel);
-        quizLabel.setText(testQuiz.getName());
+        curQuest = 0;
+        totQuest = testQuiz.getQuestions().size();
+        updateQuestioneer(testQuiz);
     }
 
     public void answerSelected(View view){
         Button answerButton = (Button) findViewById(view.getId());
         answerButton.setText("Pressed");
+    }
+
+    public void updateQuestioneer(Quiz quiz){
+        curQuest++;
+        Iterator answerIterator = ((UserQuestion)quiz.getQuestions().get(curQuest)).answerIterator(true);
+
+        TextView quizLabel = (TextView) findViewById(R.id.quizLabel);
+        TextView questNumLabel = (TextView) findViewById(R.id.questNumLabel);
+        TextView questLabel = (TextView) findViewById(R.id.questLabel);
+        TextView progressNumbers = (TextView) findViewById(R.id.progressNumbers);
+        ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        Button answer1 = (Button) findViewById(R.id.answerButton1);
+        Button answer2 = (Button) findViewById(R.id.answerButton2);
+        Button answer3 = (Button) findViewById(R.id.answerButton3);
+        Button answer4 = (Button) findViewById(R.id.answerButton4);
+
+        quizLabel.setText(quiz.getName());
+        questNumLabel.setText("Question " + curQuest);
+        questLabel.setText(((UserQuestion)quiz.getQuestions().get(curQuest)).getQuestionStr());
+        progressNumbers.setText(curQuest + " / " + totQuest);
+        progressBar.setMax(totQuest);
+        progressBar.setProgress(curQuest);
+        answer1.setText((String)((Answer)answerIterator.next()).getData());
+        answer2.setText((String)((Answer)answerIterator.next()).getData());
+        answer3.setText((String)((Answer)answerIterator.next()).getData());
+        answer4.setText((String)((Answer)answerIterator.next()).getData());
     }
 }
