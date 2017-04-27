89f1bb0c01bb22e5e37b33971b0c04d4d74c2323
Marcus Lindvärn <murk@live.se>
2017-04-27 15:07:30 +0200

    on click seems to work, the listing of the questions does not

--- app/src/main/java/com/example/murk/kwizgeeq/CreateQuiz.java
+++ app/src/main/java/com/example/murk/kwizgeeq/CreateQuiz.java
@@ -15,9 +15,11 @@ import android.widget.EditText;
 import android.widget.ListView;
 
 
+import com.example.murk.kwizgeeq.model.Answer;
 import com.example.murk.kwizgeeq.model.KwizGeeQ;
 import com.example.murk.kwizgeeq.model.Question;
 import com.example.murk.kwizgeeq.model.Quiz;
+import com.example.murk.kwizgeeq.model.UserQuestion;
 import com.example.murk.kwizgeeq.model.UserQuiz;
 
 public class CreateQuiz extends ListActivity {
@@ -27,25 +29,34 @@ public class CreateQuiz extends ListActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //teststuff
-
+        Answer testAnswer1 = new Answer(true,"fest");
+        Answer testAnswer2 = new Answer(false,"fel");
+        Answer testAnswer3 = new Answer(false,"fel");
+        Answer testAnswer4 = new Answer(false,"fel");
+        final Question testQuest = new UserQuestion("Meningen med livet jao?",null,null,null);
+        testQuest.addAnswer(testAnswer1);
+        testQuest.addAnswer(testAnswer2);
+        testQuest.addAnswer(testAnswer3);
+        testQuest.addAnswer(testAnswer4);
         Color testColor = new Color();
-        Quiz testquiz = new UserQuiz("testname", testColor);
-        KwizGeeQ.getInstance().quizzList.add(testquiz);
+        UserQuiz testquiz = new UserQuiz("testname", testColor);
+        //KwizGeeQ.getInstance().quizzList.add(testquiz);
         QuizLabel = (EditText) findViewById(R.id.QuizLabel);
-
+        testquiz.addQuestion(testQuest);
         listView = getListView();   //tha fuq?!
 
         KwizGeeQ.getInstance().activeQuiz = testquiz;
+        System.out.println(KwizGeeQ.getInstance().activeQuiz.getName());
         listAdapter = new ArrayAdapter<Question>(this, android.R.layout.simple_list_item_1, KwizGeeQ.getInstance().activeQuiz.getQuestions());
         listView.setAdapter(listAdapter);
 
         //Play quiz
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            public void onItemClick(AdapterView<?> parent, View view, int index, long id) {
 
-                Intent intent = new Intent( CreateQuiz.this, CreateQuestion.class);
-                intent.putExtra("selectedQuestion", "fest");
+                Intent intent = new Intent(CreateQuiz.this, CreateQuestion.class);
+                intent.putExtra("selectedQuestion", KwizGeeQ.getInstance().activeQuiz.getQuestions().get(index));
                 startActivity(intent);
             }
         });
