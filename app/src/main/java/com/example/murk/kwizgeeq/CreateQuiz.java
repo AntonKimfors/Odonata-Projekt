9f22cdbcefaacdf2cb4ee49633eeb67d205e614c
Marcus Lindvärn <murk@live.se>
2017-04-27 13:27:50 +0200

    Created ListView thats dependant on testQuiz created Intent for CreateQuestion

--- app/src/main/java/com/example/murk/kwizgeeq/CreateQuiz.java
+++ app/src/main/java/com/example/murk/kwizgeeq/CreateQuiz.java
@@ -1,30 +1,77 @@
 package com.example.murk.kwizgeeq;
 
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.graphics.Color;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 
-public class CreateQuiz extends AppCompatActivity {
 
+import com.example.murk.kwizgeeq.model.KwizGeeQ;
+import com.example.murk.kwizgeeq.model.Question;
+import com.example.murk.kwizgeeq.model.Quiz;
+import com.example.murk.kwizgeeq.model.UserQuiz;
+
+public class CreateQuiz extends ListActivity {
+    public EditText QuizLabel;
+    ListView listView;
+    ArrayAdapter<Question> listAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_quiz);
-        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
+        //teststuff
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Color testColor = new Color();
+        Quiz testquiz = new UserQuiz("testname", testColor);
+        KwizGeeQ.getInstance().quizzList.add(testquiz);
+        QuizLabel = (EditText) findViewById(R.id.QuizLabel);
+
+        listView = getListView();   //tha fuq?!
+
+        KwizGeeQ.getInstance().activeQuiz = testquiz;
+        listAdapter = new ArrayAdapter<Question>(this, android.R.layout.simple_list_item_1, KwizGeeQ.getInstance().activeQuiz.getQuestions());
+        listView.setAdapter(listAdapter);
+
+        //Play quiz
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View view) {
-                setContentView(R.layout.activity_create_question);
-                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                //        .setAction("Action", null).show();
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Intent intent = new Intent( CreateQuiz.this, CreateQuestion.class);
+                intent.putExtra("selectedQuestion", "fest");
+                startActivity(intent);
             }
         });
+
+
+
+
+
+        //end teststuff
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_quiz);
+
+    }
+    public void fabPressed(View view){
+        Intent intent = new Intent(this, CreateQuestion.class);
+
+        startActivity(intent);
+
+        Bundle applesData = getIntent().getExtras();
+        if(applesData==null){
+            return;
+        }
+        String dataRecieved = applesData.getString("stufftosend");
+
+
     }
 
 }
