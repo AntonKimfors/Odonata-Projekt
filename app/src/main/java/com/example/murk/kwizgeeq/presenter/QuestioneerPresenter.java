64f1e6ff05bd60eb283cd9db41340ea104b51124
Are <are.ehnberg@outlook.com>
2017-05-03 18:00:54 +0200

    Added basic implemented functions and constructor to QuestioneerPresenter

--- app/src/main/java/com/example/murk/kwizgeeq/presenter/QuestioneerPresenter.java
+++ app/src/main/java/com/example/murk/kwizgeeq/presenter/QuestioneerPresenter.java
@@ -1,11 +1,36 @@
 package com.example.murk.kwizgeeq.presenter;
 
+import com.example.murk.kwizgeeq.model.KwizGeeQ;
+import com.example.murk.kwizgeeq.view.QuestioneerView;
+
 /**
  * Created by Are on 03/05/2017.
  */
 
 public class QuestioneerPresenter implements Presenter{
 
+    private QuestioneerView view;
+    private KwizGeeQ model;
 
+    public QuestioneerPresenter(QuestioneerView view) {
+        this.view = view;
+        this.model = KwizGeeQ.getInstance();
+    }
+
+    public void onCreate() {
+        model = KwizGeeQ.getInstance();
+    }
+
+    public void onPause() {
+
+    }
+
+    public void onResume() {
+
+    }
+
+    public void onDestroy() {
+
+    }
 
 }
