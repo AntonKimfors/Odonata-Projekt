c4f2bc111326be648cdef620747176611401e263
Marcus Lindvärn <murk@live.se>
2017-05-06 11:31:51 +0200

    non MVC implementation of dialogs completed

--- app/src/main/java/com/example/murk/kwizgeeq/QuizList.java
+++ app/src/main/java/com/example/murk/kwizgeeq/QuizList.java
@@ -5,12 +5,18 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.PointF;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import com.example.murk.kwizgeeq.activity.EditQuizActivity;
 import com.example.murk.kwizgeeq.model.Answer;
 import com.example.murk.kwizgeeq.model.KwizGeeQ;
 import com.example.murk.kwizgeeq.model.QuizListAdapter;
@@ -18,8 +24,11 @@ import com.example.murk.kwizgeeq.model.UserQuestion;
 import com.example.murk.kwizgeeq.model.UserQuiz;
 import com.example.murk.kwizgeeq.activity.QuestioneerActivity;
 
-public class QuizList extends ListActivity {
+import org.xdty.preference.colorpicker.ColorPickerDialog;
+import org.xdty.preference.colorpicker.ColorPickerSwatch;
 
+public class QuizList extends ListActivity {
+    private int mSelectedColor;
     ListView listView;
     QuizListAdapter adapter;
 
@@ -103,8 +112,69 @@ public class QuizList extends ListActivity {
     }
 
     public void fabPressed(View view){
-        Intent intent = new Intent(this, CreateQuiz.class);
-        startActivity(intent);
+
+
+
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(QuizList.this);
+        View mView = getLayoutInflater().inflate(R.layout.dialog_create_quiz, null);
+        final EditText mEmail = (EditText) mView.findViewById(R.id.etQuizName);
+        Button mCancel = (Button) mView.findViewById(R.id.btnCancel);
+        Button mCreateQuiz = (Button) mView.findViewById(R.id.btnCreateQuiz);
+        final Button mPickColor = (Button) mView.findViewById(R.id.btnPickColor);
+
+
+
+
+        mBuilder.setView(mView);
+        final AlertDialog dialog = mBuilder.create();
+        dialog.show();
+
+        mPickColor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mSelectedColor = ContextCompat.getColor(QuizList.this, R.color.flamingo);
+
+
+                int[] mColors = getResources().getIntArray(R.array.default_rainbow);
+
+                ColorPickerDialog dialog = ColorPickerDialog.newInstance(R.string.color_picker_default_title,
+                        mColors,
+                        mSelectedColor,
+                        5, // Number of columns
+                        ColorPickerDialog.SIZE_SMALL,
+                        true // True or False to enable or disable the serpentine effect
+                        //0, // stroke width
+                        //Color.BLACK // stroke color
+                );
+
+                dialog.setOnColorSelectedListener(new ColorPickerSwatch.OnColorSelectedListener() {
+
+                    @Override
+                    public void onColorSelected(int color) {
+                        mSelectedColor = color;
+                        mPickColor.setBackgroundColor(mSelectedColor);
+
+                    }
+
+                });
+
+                dialog.show(getFragmentManager(), "color_dialog_test");
+            }
+        });
+
+        mCreateQuiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(QuizList.this, CreateQuiz.class);
+                startActivity(intent);
+            }
+        });
+        mCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
     }
 
 
