44122aefb1731032aabad71d7c8450657702a98b
Murk818 <murk@live.se>
2017-05-09 19:57:58 +0200

    Base Activity created for EditQuizActivity

--- app/src/main/java/com/example/murk/kwizgeeq/activity/EditQuizActivity.java
+++ app/src/main/java/com/example/murk/kwizgeeq/activity/EditQuizActivity.java
@@ -1,78 +1,30 @@
 package com.example.murk.kwizgeeq.activity;
 
 import android.app.ListActivity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.graphics.Color;
-import android.widget.AdapterView;
 import android.widget.EditText;
-import android.widget.ListView;
 
-
+import com.example.murk.kwizgeeq.controller.EditQuizController;
 import com.example.murk.kwizgeeq.view.*;
 import com.example.murk.kwizgeeq.R;
-import com.example.murk.kwizgeeq.model.*;
 
-public class EditQuizActivity extends ListActivity {
-    private EditText QuizLabel;
-    ListView listView;
-    CreateQuizAdapter adapter;
+public class EditQuizActivity extends ListActivity implements NavigatableActivity {
+
+
+    private EditQuizController controller;
+    private EditQuizView view;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        //Flyttade bara upp dessa för att få det att funka. Test koden gör ju
-        // så att test Question alltid finns där med när man skapar ett nytt quiz
-        //vilket verkar funka nu
-        //
-        //Se QuizListAdapter för att få listan att se ut på annat sätt
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_quiz);
+        setTheme(R.style.AppTheme_NoActionBar);
+        setContentView(R.layout.activity_quiz_list);
+        int index = getIntent().getIntExtra("quizindex", 0);
 
-        //teststuff
-        Answer testAnswer1 = new Answer(true,"fest");
-        Answer testAnswer2 = new Answer(false,"fel");
-        Answer testAnswer3 = new Answer(false,"fel");
-        Answer testAnswer4 = new Answer(false,"fel");
-        final Question testQuest = new UserQuestion("Meningen med livet jao?",null,0,0,null);
-        testQuest.addAnswer(testAnswer1);
-        testQuest.addAnswer(testAnswer2);
-        testQuest.addAnswer(testAnswer3);
-        testQuest.addAnswer(testAnswer4);
-        Color testColor = new Color();
-        UserQuiz testquiz = new UserQuiz("testname", testColor);
-        testquiz.addQuestion(testQuest);
-        KwizGeeQ.getInstance().activeQuiz = testquiz;
-        //KwizGeeQ.getInstance().quizzList.add(testquiz);
-        //end teststuff
-
-        QuizLabel = (EditText) findViewById(R.id.QuizLabel);
-
-
-        listView = getListView();
-
-        adapter = new CreateQuizAdapter(this, KwizGeeQ.getInstance().activeQuiz.getQuestions());
-        listView.setAdapter(adapter);
-
-        //Edit Question
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int index, long id) {
-
-                Intent intent = new Intent(EditQuizActivity.this, CreateQuestionActivity.class);
-                intent.putExtra("selectedQuestion", KwizGeeQ.getInstance().activeQuiz.getQuestions().get(index));
-                startActivity(intent);
-            }
-        });
-        // end onCreate
-    }
-    public void fabPressed(View view){
-        Intent intent = new Intent(EditQuizActivity.this, CreateQuestionActivity.class);
-
-        startActivity(intent);
-
-
+        view = new EditQuizView(index, getListView(), this, this, (EditText) findViewById(R.id.etQuizName));
+        controller = new EditQuizController(view);
+        view.addObserver(controller);
+        controller.onCreate();
 
     }
-
 }
