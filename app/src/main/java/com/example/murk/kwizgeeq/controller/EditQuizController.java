a3fc4bb46d6ba91856ffe6176e5d2cb622233f00
Murk818 <murk@live.se>
2017-05-09 19:55:33 +0200

    Created Base Controller functions

--- app/src/main/java/com/example/murk/kwizgeeq/controller/EditQuizController.java
+++ app/src/main/java/com/example/murk/kwizgeeq/controller/EditQuizController.java
@@ -1,8 +1,58 @@
 package com.example.murk.kwizgeeq.controller;
 
+import android.graphics.Color;
+
+import com.example.murk.kwizgeeq.model.Answer;
+import com.example.murk.kwizgeeq.model.KwizGeeQ;
+import com.example.murk.kwizgeeq.model.UserQuestion;
+import com.example.murk.kwizgeeq.model.UserQuiz;
+import com.example.murk.kwizgeeq.view.EditQuizView;
+import com.example.murk.kwizgeeq.view.QuizListView;
+
+import java.util.Observable;
+import java.util.Observer;
+
 /**
  * Created by Murk on 2017-05-06.
  */
 
-public class EditQuizController {
+public class EditQuizController implements Controller, Observer {
+
+    private EditQuizView view;
+    private KwizGeeQ model;
+
+    public EditQuizController(EditQuizView view){
+        this.view = view;
+        model = KwizGeeQ.getInstance();
+
+    }
+
+
+
+    @Override
+    public void onCreate() {
+
+    }
+
+    @Override
+    public void onPause() {
+
+    }
+
+    @Override
+    public void onResume() {
+
+    }
+
+    @Override
+    public void onDestroy() {
+
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+
+    }
 }
+
+
