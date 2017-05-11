b99a410fad3443acc96c190ed8638076a96b3ad6
AntonKimfors <antonkimfors@hotmail.com>
2017-05-11 16:12:05 +0200

    FAB button + fixed old code

--- app/src/main/java/com/example/murk/kwizgeeq/view/QuizListView.java
+++ app/src/main/java/com/example/murk/kwizgeeq/view/QuizListView.java
@@ -4,16 +4,23 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.ListView;
 
+import com.example.murk.kwizgeeq.R;
 import com.example.murk.kwizgeeq.activity.EditQuizActivity;
 import com.example.murk.kwizgeeq.activity.NavigatableActivity;
 import com.example.murk.kwizgeeq.activity.QuestioneerActivity;
 import com.example.murk.kwizgeeq.model.KwizGeeQ;
+import com.example.murk.kwizgeeq.model.Quiz;
 import com.example.murk.kwizgeeq.model.QuizListAdapter;
+import com.example.murk.kwizgeeq.model.UserQuiz;
 
 import java.util.Observable;
 
@@ -23,16 +30,18 @@ import java.util.Observable;
 
 public class QuizListView extends Observable {
 
-    public ListView listView;
-
+    private ListView listView;
+    private NavigatableActivity currentActivity;
     private KwizGeeQ model;
     private QuizListAdapter adapter;
+    private FloatingActionButton fab;
 
 
-    public QuizListView(final ListView listView, final Context context, final NavigatableActivity oldActivity){
+    public QuizListView(final ListView listView, final Context context, final NavigatableActivity currentActivity, FloatingActionButton fab){
         this.listView = listView;
         this.model = KwizGeeQ.getInstance();
-
+        this.currentActivity = currentActivity;
+        this.fab = fab;
 
 
         this.adapter = new QuizListAdapter(context, model.getQuizList());
@@ -43,11 +52,13 @@ public class QuizListView extends Observable {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-                //KwizGeeQ.getInstance().activeQuiz = KwizGeeQ.getInstance().quizList.get(position); //Make the clicked quiz active quiz.
+                //KwizGeeQ.getInstance().activeQuiz = KwizGeeQ.getInstance().quizzList.get(position); //Make the clicked quiz active quiz.
 
                 Intent intent = new Intent(context, QuestioneerActivity.class);
+
                 intent.putExtra("quizIndex", position);
-                oldActivity.startActivity(intent);
+                currentActivity.startActivity(intent);
+
             }
         });
 
@@ -56,50 +67,95 @@ public class QuizListView extends Observable {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
 
-                //KwizGeeQ.getInstance().activeQuiz = KwizGeeQ.getInstance().quizList.get(position);
+                KwizGeeQ.getInstance().activeQuiz = KwizGeeQ.getInstance().getQuizList().get(position);
 
-                AlertDialog.Builder alertDialog = new AlertDialog.Builder((Activity) oldActivity)
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder((Activity) currentActivity)
                         .setIcon(android.R.drawable.ic_dialog_alert)
                         .setTitle("Edit Quiz?")
-                        .setMessage("Do you want to Edit or Delete the quiz?")
+                        .setMessage("Do you want to Edit or Delete the quiz?");
 
-                        .setPositiveButton("Edit", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                Intent intent = new Intent(context, EditQuizActivity.class);
-                                oldActivity.startActivity(intent);
-                            }
-                        })
+                AlertDialog ad = alertDialog.create();
+
+                alertDialog.setPositiveButton("Edit", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(context, EditQuizActivity.class);
+                        currentActivity.startActivity(intent);
+                    }
+                })
 
                         .setNegativeButton("DELETE", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
                                 model.getQuizList().remove(position);
-                                oldActivity.finish();
-                                oldActivity.startActivity(((Activity) oldActivity).getIntent());
+
+
+                                currentActivity.finish();
+                                currentActivity.startActivity(((Activity) currentActivity).getIntent());
 
 
 
                             }
-                        });
-
-                alertDialog.create().show();
-
-
+                        })
+                        .show();
+                ;
 
 
-                //Intent intent = new Intent(context, EditQuizActivity.class);
-                //oldActivity.startActivity(intent);
+                //AlertDialog ad = alertDialog.create();
+                //ad.show();
+
+                //ad.dismiss(); For wheb clicking cancel or similar
+
+
+
 
                 return true;
             }
         });
 
+        //FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        this.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+
+            public void onClick(View view) {
+
+                final EditText input = new EditText((Activity) currentActivity);
 
 
+                final AlertDialog.Builder alertDialog = new AlertDialog.Builder((Activity) currentActivity)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setTitle("Create Quiz?")
+                        .setView(input);
+
+                final AlertDialog ad = alertDialog.create();
+
+
+                alertDialog.setPositiveButton("Create", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //Intent intent = new Intent(context, EditQuizActivity.class);
+                        //currentActivity.startActivity(intent);
+
+                        String quizTitle = input.getText().toString();
+                        model.getQuizList().add(new UserQuiz(quizTitle, 0));
+                    }
+                })
+
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                                ad.dismiss();
+                            }
+                        })
+                        .show();
+                ;
+
+
+            }
+        });
     }
-
-
-
-
 }
+
+
