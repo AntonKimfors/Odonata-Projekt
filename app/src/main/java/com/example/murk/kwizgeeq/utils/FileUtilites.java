301a8cd1b74683d3ed6bdd55bcee85d348ae46b5
AntonKimfors <antonkimfors@hotmail.com>
2017-05-12 12:13:33 +0200

    StorageType and FileUtilities created and some of their methods

---
+++ app/src/main/java/com/example/murk/kwizgeeq/utils/FileUtilites.java
@@ -0,0 +1,63 @@
+package com.example.murk.kwizgeeq.utils;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * Created by akimfors on 2017-05-12.
+ */
+
+public class FileUtilites {
+
+    public static void saveAssetImage(Context context, String assetName) {
+        File fileDirectory = context.getFilesDir();
+        File fileToWrite = new File(fileDirectory, assetName);
+
+        AssetManager assetManager = context.getAssets();
+        try {
+            InputStream in = assetManager.open(assetName);
+            FileOutputStream out = new FileOutputStream(fileToWrite);
+            copyFile(in ,out);
+            in.close();
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //FileOutputStream outOld = context.openFileOutput(fileToWrite.getAbsolutePath(), Context.MODE_PRIVATE);
+    }
+
+    private static void copyFile(InputStream in, OutputStream out) throws IOException{
+        byte[] buffer = new byte[1024];
+        int read;
+        while((read = in.read(buffer)) != -1){
+            out.write(buffer, 0, read);
+        }
+    }
+
+    public static void saveImage(Context context, Bitmap bitmap, String name) {
+        File fileDirectory = context.getFilesDir();
+        File fileToWrite = new File(fileDirectory, name);
+
+        try {
+            FileOutputStream outputStream = new FileOutputStream(fileToWrite);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+            outputStream.flush();
+            outputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
