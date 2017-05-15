1f364961a6a14d916e74ffba8aaa65976816b09b
gaffelman <henrikhakansson95@gmail.com>
2017-05-15 14:18:13 +0200

    Fixed bug when creating new quiz. Questions can be created.

--- app/src/main/java/com/example/murk/kwizgeeq/view/EditQuizView.java
+++ app/src/main/java/com/example/murk/kwizgeeq/view/EditQuizView.java
@@ -110,7 +110,7 @@ public class EditQuizView extends Observable {
             public void onClick(View view) {
                 Intent intent = new Intent(context, createQuestionActivity);
                 //model.getQuiz(index).
-                intent.putExtra("quizIndex",1);  //TODO checkthis model.getQuestionList(index).size()
+                intent.putExtra("quizIndex",model.getQuizList().size()-1);  //TODO checkthis model.getQuestionList(index).size()
                 oldActivity.startActivity(intent);
 
             }
