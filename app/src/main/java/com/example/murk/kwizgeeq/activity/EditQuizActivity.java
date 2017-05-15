1d2a62c4d4653c335bc3badf1b46d9fd069b5aec
Marcus Lindvärn <murk@live.se>
2017-05-15 08:15:39 +0200

    added the Button to change color in the Creation of the editQuizView

--- app/src/main/java/com/example/murk/kwizgeeq/activity/EditQuizActivity.java
+++ app/src/main/java/com/example/murk/kwizgeeq/activity/EditQuizActivity.java
@@ -3,6 +3,7 @@ package com.example.murk.kwizgeeq.activity;
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.murk.kwizgeeq.controller.EditQuizController;
@@ -23,7 +24,7 @@ public class EditQuizActivity extends ListActivity {
         int index = getIntent().getIntExtra("quizIndex",0);
 
 
-        view = new EditQuizView(EditQuestionActivity.class,index, getListView(), this, this, (EditText) findViewById(R.id.etQuizLabel),(FloatingActionButton) findViewById(R.id.fab));
+        view = new EditQuizView(EditQuestionActivity.class,index, getListView(), this, this, (EditText) findViewById(R.id.etQuizLabel),(FloatingActionButton) findViewById(R.id.fab),(Button) findViewById(R.id.btnColorPicker));
 
         controller = new EditQuizController(view);
         view.addObserver(controller);
