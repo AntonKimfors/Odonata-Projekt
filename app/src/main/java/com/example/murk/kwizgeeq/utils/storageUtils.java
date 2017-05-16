08ffac643239847f248137f03f63356c622519e3
AntonKimfors <antonkimfors@hotmail.com>
2017-05-16 09:47:12 +0200

    fixed saveQuizList in storageUtils

--- app/src/main/java/com/example/murk/kwizgeeq/utils/storageUtils.java
+++ app/src/main/java/com/example/murk/kwizgeeq/utils/storageUtils.java
@@ -2,14 +2,57 @@ package com.example.murk.kwizgeeq.utils;
 
 import android.content.Context;
 import android.os.Environment;
+import android.util.Log;
 
+import com.example.murk.kwizgeeq.model.KwizGeeQ;
+import com.example.murk.kwizgeeq.model.Quiz;
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 /**
  * Created by akimfors on 2017-05-16.
  */
 
 public class storageUtils {
+
+
+
+    public static void saveQuizList(Context context) {
+        //TODO: Try saving the data
+
+        ArrayList<Quiz> quizlist = KwizGeeQ.getInstance().getQuizList();
+
+        Gson gson = new Gson();
+        String jsonquizlist = gson.toJson(quizlist);
+
+        try
+        {
+            File quizFile = new File(context.getFilesDir(), "quiz.txt");
+            FileWriter fileWriter = new FileWriter(quizFile, false);
+            BufferedWriter writer = new BufferedWriter((fileWriter));
+            writer.write(jsonquizlist);
+            writer.close();
+        }
+        catch (Exception e)
+        {
+            Log.e("Persistance", "Error saving file " + e.getMessage());
+        }
+    }
+
+    public static void saveImage(Context context, String imageName){
+        File fileDirectory = getFileDirectory(context);
+        //File fileToWrite = new
+
+    }
+
 
     public static File getFileDirectory(Context context) {
         String storageType = StorageType.PRIVATE_EXTERNAL; //Choose what sort of storage type
