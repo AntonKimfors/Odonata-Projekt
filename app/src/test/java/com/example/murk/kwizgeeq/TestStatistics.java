b8edb98835b9f8dfe2723100f5ed3994d8c57ace
Are <are.ehnberg@outlook.com>
2017-05-26 18:01:13 +0200

    Created TestStatistics and added pertinent testing methods (more may be added later if necessary).

---
+++ app/src/test/java/com/example/murk/kwizgeeq/TestStatistics.java
@@ -0,0 +1,47 @@
+package com.example.murk.kwizgeeq;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * Created by Are on 26/05/2017.
+ */
+
+public class TestStatistics {
+
+    @BeforeClass
+    public static void beforeClass(){
+
+    }
+
+    @Test
+    public void testStatisticsCreation(){
+
+    }
+
+    @Test
+    public void testStatisticsQuizIncremention(){
+
+    }
+
+    @Test
+    public void testStatisticsQuestionIncremention(){
+
+    }
+
+    @Test
+    public void testStatisticsAnswerIncremention(){
+
+    }
+
+    @Test
+    public void testStatisticsTimer(){
+
+    }
+
+    @Test
+    public void testStatisticsMerging(){
+
+    }
+
+}
