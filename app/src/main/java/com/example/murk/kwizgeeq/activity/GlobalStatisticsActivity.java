a5b35d30471a23a4286a28e21c42b7cabb5bcfc0
Are <are.ehnberg@outlook.com>
2017-05-27 15:58:24 +0200

    Implemented showing of all global stats in global statistics view.

--- app/src/main/java/com/example/murk/kwizgeeq/activity/GlobalStatisticsActivity.java
+++ app/src/main/java/com/example/murk/kwizgeeq/activity/GlobalStatisticsActivity.java
@@ -4,13 +4,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.murk.kwizgeeq.R;
+import com.example.murk.kwizgeeq.controller.GlobalStatisticsController;
+import com.example.murk.kwizgeeq.view.GlobalStatisticsView;
 
 public class GlobalStatisticsActivity extends AppCompatActivity {
+
+    private GlobalStatisticsController controller;
+    private GlobalStatisticsView view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_global_statistics);
+
+        view = new GlobalStatisticsView(this);
+        controller = new GlobalStatisticsController(this, view);
     }
 
 }
--- app/src/main/java/com/example/murk/kwizgeeq/controller/GlobalStatisticsController.java
+++ app/src/main/java/com/example/murk/kwizgeeq/controller/GlobalStatisticsController.java
@@ -1,11 +1,23 @@
 package com.example.murk.kwizgeeq.controller;
 
+import android.app.Activity;
+
+import com.example.murk.kwizgeeq.model.Statistics;
+import com.example.murk.kwizgeeq.view.GlobalStatisticsView;
+
 /**
  * Created by Are on 27/05/2017.
  */
 
 public class GlobalStatisticsController {
 
+    private GlobalStatisticsView view;
+    private Activity activity;
 
+    public GlobalStatisticsController(Activity activity, GlobalStatisticsView view) {
+        this.activity = activity;
+        this.view = view;
+        view.setupStatistics((Statistics)activity.getIntent().getSerializableExtra("statistics"));
+    }
 
 }
