2fec456af004a2c724e5437403261fdffe1bcea2
Marcus Lindvärn <murk@live.se>
2017-05-28 22:35:14 +0200

    Pre test stuff added

---
+++ app/src/test/java/com/kwizgeeq/KwizGeeQTest.java
@@ -0,0 +1,54 @@
+package com.kwizgeeq;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.net.Uri;
+
+import com.kwizgeeq.model.Answer;
+import com.kwizgeeq.model.AnswerType;
+import com.kwizgeeq.model.Question;
+import com.kwizgeeq.model.Statistics;
+import com.kwizgeeq.model.UserQuiz;
+
+import org.junit.Test;
+
+import java.util.Iterator;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Murk on 2017-05-28.
+ */
+
+public class KwizGeeQTest {
+    Activity activity;
+    UserQuiz quiz = new UserQuiz("PartyQuiz!", Color.CYAN);
+    Answer answer1 = new Answer(true, "Smurfen", AnswerType.TEXT);
+    Answer answer2 = new Answer(false, "Trump", AnswerType.TEXT);
+    Answer answer3 = new Answer(false, "en Sten", AnswerType.TEXT);
+    Answer answer4 = new Answer(false, "Anders Svensson Lindberg", AnswerType.TEXT);
+    Question question = new Question();
+    Statistics statistics = new Statistics();
+
+
+    @Test
+    public void testBasicFunctionality() {
+        //START ---------------------------------- Statistics
+        assertTrue(statistics.getQuizCount() == 0);
+        assertTrue(statistics.getQuestionCount() == 0);
+        assertTrue(statistics.getAnswerCorrectCount() == 0);
+        assertTrue(statistics.getAnswerIncorrectCount() == 0);
+        assertTrue(statistics.getSecondsSpent() == 0);
+
+        question.setQuestionText("vad är IT-sektionens Helgon?");
+        question.setImagePath("finabilder/annat");
+        question.addAnswer(answer1.getData(), answer1.isCorrect(), answer1.getAnswerType());
+        question.addAnswer(answer2.getData(), answer2.isCorrect(), answer2.getAnswerType());
+        question.addAnswer(answer3.getData(), answer3.isCorrect(), answer3.getAnswerType());
+        question.addAnswer(answer4.getData(), answer4.isCorrect(), answer4.getAnswerType());
+
+
+
+
+    }
+}
